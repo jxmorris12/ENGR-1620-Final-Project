3dcf2506bd4166e47f465c89eb6ac8d8db919427
jxmorris12 <jxmorris12@gmail.com>
2016-04-20 15:17:39 -0400

    trying to add mouseover label

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ loadBuildings();
 // loadPenguins();
 
 
-var step = 0; var numOfSteps = buildings.length;
+
+var step = 0; 
+var numOfSteps = buildings.length;
 
 function loadPenguins() {
   loadSVGFromAddress("https://rawgit.com/VengadoraVG/moving-to-gnulinux/master/img/tux.svg", "");
@@ -34,7 +36,6 @@ function loadBuildings() {
   var addressBase = "final project/vectors/";
   for(var i in buildings) {
     var buildingName = buildings[i];
-    console.log('loading building', String(i)+':', buildingName);
     loadSVGFromAddress(addressBase, buildingName);
   }
 }
@@ -50,32 +51,11 @@ function loadSVGFromAddress(base, name) {
     var importedNode = $( document.importNode(xml.documentElement, true) );
     var path = importedNode.find("path");
     //
+    var name = path.attr("id");
+    path.append("title").text(name);
+    //
     svg[0].appendChild(path[0].cloneNode(true));
+    //
     step++;
   });
-}
-
-function getRandomColor() {
-  return "#"+((1<<24)*Math.random()|0).toString(16); // thanks Stack Overflow this is sick
-}
-
-function rainbow(numOfSteps, step) {
-    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
-    // Adam Cole, 2011-Sept-14
-    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-    var r, g, b;
-    var h = step / numOfSteps;
-    var i = ~~(h * 6);
-    var f = h * 6 - i;
-    var q = 1 - f;
-    switch(i % 6){
-        case 0: r = 1; g = f; b = 0; break;
-        case 1: r = q; g = 1; b = 0; break;
-        case 2: r = 0; g = 1; b = f; break;
-        case 3: r = 0; g = q; b = 1; break;
-        case 4: r = f; g = 0; b = 1; break;
-        case 5: r = 1; g = 0; b = q; break;
-    }
-    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
-    return (c);
 }
